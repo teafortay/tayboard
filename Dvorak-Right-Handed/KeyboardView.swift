e9e526c5f9f71f7ae8d1462758fc8d318bacdbee
Taylor Shaw <tay@taylorcodeshop.com>
2021-07-09 20:43:58 -0400

    created xib with 1 button

---
+++ Dvorak-Right-Handed/KeyboardView.swift
@@ -0,0 +1,24 @@
+//
+//  KeyboardView.swift
+//  Dvorak-Right-Handed
+//
+//  Created by Taylor Shaw on 7/9/21.
+//  Copyright © 2021 Taylor Shaw. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardView: UIView {
+
+    @IBOutlet var contentView: UIView!
+    
+    @IBOutlet weak var key1: UIButton!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
