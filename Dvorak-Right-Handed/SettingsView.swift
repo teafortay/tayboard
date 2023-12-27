c198b04c785e124aedeb721bd344422d2aae1c85
Taylor Shaw <tay@taylorcodeshop.com>
2023-12-27 18:52:15 -0500

    created empty xib for settings view

---
+++ Dvorak-Right-Handed/SettingsView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsView.swift
+//  Dvorak-Right-Handed
+//
+//  Created by Taylor Shaw on 12/27/23.
+//  Copyright © 2023 Taylor Shaw. All rights reserved.
+//
+
+import UIKit
+
+class SettingsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
