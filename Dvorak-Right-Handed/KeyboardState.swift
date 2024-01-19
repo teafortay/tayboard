8f8ec8b614e6435acb36f69b2f0b6f3fc6a854c0
Taylor Shaw <tay@taylorcodeshop.com>
2024-01-19 15:26:11 -0500

    using scrollview example from stackoverflow + minor edit in state

--- Dvorak-Right-Handed/KeyboardState.swift
+++ Dvorak-Right-Handed/KeyboardState.swift
@@ -29,7 +29,8 @@ struct Setting {
 }
 
 struct Settings {
-    var enableCaps = Setting(name: "Enable caps lock", detail: nil, value: true)
+    //MARK: settings
+    var enableCapsLock = Setting(name: "Enable caps lock", detail: nil, value: true)
     var periodShortcut =  Setting(name: "\".\" shortcut", detail: "Double tap Space to insert a .", value: true)
     var haptics = Setting(name: "Haptics", detail: "You also need to 'Enable Full Access' for this to work", value: false)
     var keyboardClicks = Setting(name: "Keyboard clicks", detail: "You also need to 'Enable Full Access' for this to work", value: false)
--- Dvorak-Right-Handed/Views/SettingsView/SettingsView.swift
+++ Dvorak-Right-Handed/Views/SettingsView/SettingsView.swift
@@ -12,28 +12,78 @@ class SettingsView: UIView {
     var settings: Settings
     var save: (Settings) -> Void
     
+    //MARK: settings
+    var enableCapsLockSwitch = UISwitch()
+    var periodShortcutSwitch = UISwitch()
+    var hapticsSwitch = UISwitch()
+    var keyboardClicksSwitch = UISwitch()
+    
+    
     init(frame: CGRect, settings: Settings, callback: @escaping (Settings) -> Void) {
         self.settings = settings
         self.save = callback
         super.init(frame: frame)
+        addSubview(scrollView)
+        scrollView.addSubview(scrollViewContainer)
+        scrollViewContainer.addArrangedSubview(redView)
+        scrollViewContainer.addArrangedSubview(blueView)
+        scrollViewContainer.addArrangedSubview(greenView)
         
-        let table = UITableView()
-        table.frame = self.bounds
-        self.addSubview(table)
+        scrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         
-        var frame = CGRect(x: 10, y: 100, width: 300, height: 50)
-        frame.origin.y += 50
-        frame.origin.x = 10
-        frame.size.width = 300
-        let closeButon = UIButton(frame: frame)
-        closeButon.setTitle("Close", for: .normal)
-        closeButon.backgroundColor = .magenta
-        closeButon.addTarget(
-            self,
-            action: #selector(closePressed),
-            for: .touchUpInside)
-        self.addSubview(closeButon)
+        scrollViewContainer.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        scrollViewContainer.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        scrollViewContainer.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        scrollViewContainer.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        // this is important for scrolling
+        scrollViewContainer.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
     }
+        let scrollView: UIScrollView = {
+            let scrollView = UIScrollView()
+            
+            scrollView.translatesAutoresizingMaskIntoConstraints = false
+            return scrollView
+        }()
+        
+        let scrollViewContainer: UIStackView = {
+            let view = UIStackView()
+            
+            view.axis = .vertical
+            view.spacing = 10
+            
+            view.translatesAutoresizingMaskIntoConstraints = false
+            return view
+        }()
+        
+        let redView: UIView = {
+            let view = UIView()
+            view.heightAnchor.constraint(equalToConstant: 500).isActive = true
+            view.backgroundColor = .red
+            return view
+        }()
+        
+        let blueView: UIView = {
+            let view = UIView()
+            view.heightAnchor.constraint(equalToConstant: 200).isActive = true
+            view.backgroundColor = .blue
+            return view
+        }()
+        
+        let greenView: UIView = {
+            let view = UIView()
+            view.heightAnchor.constraint(equalToConstant: 1200).isActive = true
+            view.backgroundColor = .green
+            return view
+        }()
+        //        closeButon.addTarget(
+        //            self,
+        //            action: #selector(closePressed),
+        //            for: .touchUpInside)
+    
+
     
     required init?(coder: NSCoder) {
 //        super.init(coder: coder)
