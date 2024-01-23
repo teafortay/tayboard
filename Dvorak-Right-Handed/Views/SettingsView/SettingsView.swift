6072d0f85230e151617a861f4b470df98469c3ae
Taylor Shaw <tay@taylorcodeshop.com>
2024-01-22 19:18:28 -0500

    added header label and other settings and gap at the bottom

--- Dvorak-Right-Handed/Views/SettingsView/SettingsView.swift
+++ Dvorak-Right-Handed/Views/SettingsView/SettingsView.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Taylor Shaw on 12/27/23.
 //  Copyright © 2023 Taylor Shaw. All rights reserved.
+// referenced this to mimic a UITableView w/o UIViewController: https://stackoverflow.com/questions/29094129/create-a-vertical-uiscrollview-programmatically-in-swift/54860024#54860024
 //
 
 import UIKit
@@ -23,16 +24,34 @@ class SettingsView: UIView {
         self.settings = settings
         self.save = callback
         super.init(frame: frame)
+        
+        //header label
+        let header = UILabel()
+        header.text = "Settings"
+        header.font = UIFont.boldSystemFont(ofSize: 16)
+        addSubview(header)
+        header.translatesAutoresizingMaskIntoConstraints = false
+        header.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
+        header.topAnchor.constraint(equalTo: self.topAnchor, constant: 5).isActive = true
+        
         addSubview(scrollView)
         scrollView.addSubview(scrollViewContainer)
-        scrollViewContainer.addArrangedSubview(drawSetting(setting: settings.enableCapsLock, sw: self.enableCapsLockSwitch))
+        let caps = drawSetting(setting: settings.enableCapsLock, sw: enableCapsLockSwitch)
+        scrollViewContainer.addArrangedSubview(caps)
+        let shortcut = drawSetting(setting: settings.periodShortcut, sw: periodShortcutSwitch)
+        scrollViewContainer.addArrangedSubview(shortcut)
+        let haptics = drawSetting(setting: settings.haptics, sw: hapticsSwitch)
+        scrollViewContainer.addArrangedSubview(haptics)
+        let sound = drawSetting(setting: settings.keyboardClicks, sw: keyboardClicksSwitch)
+        scrollViewContainer.addArrangedSubview(sound)
+        scrollViewContainer.addArrangedSubview(redView)
         scrollViewContainer.addArrangedSubview(blueView)
         scrollViewContainer.addArrangedSubview(greenView)
         
         scrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         scrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-        scrollView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        scrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: self.topAnchor, constant: 30).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -30).isActive = true
         
         scrollViewContainer.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
         scrollViewContainer.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
@@ -52,7 +71,7 @@ class SettingsView: UIView {
             let view = UIStackView()
             
             view.axis = .vertical
-            view.spacing = 10
+            view.spacing = 1
             
             view.translatesAutoresizingMaskIntoConstraints = false
             return view
@@ -70,21 +89,20 @@ class SettingsView: UIView {
         
         sw.setOn(setting.value, animated: false)
         sw.translatesAutoresizingMaskIntoConstraints = false
-//        sw.contentHorizontalAlignment = .trailing
         
         view.backgroundColor = .systemBackground
         view.heightAnchor.constraint(equalToConstant: 50).isActive = true
         view.addSubview(name)
         view.addSubview(sw)
         
-        name.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
-        sw.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
+        name.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        sw.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
         return view
     }
         
         let redView: UIView = {
             let view = UIView()
-            view.heightAnchor.constraint(equalToConstant: 500).isActive = true
+            view.heightAnchor.constraint(equalToConstant: 50).isActive = true
             view.backgroundColor = .red
             return view
         }()
