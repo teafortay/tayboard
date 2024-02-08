3dae0fe99d4f3e2ced30cc1d1925a9bfc1c1bc61
Taylor Shaw <tay@taylorcodeshop.com>
2024-02-08 16:27:21 -0500

    updated text on container app screen, idk if I like it

--- TheRightDvorak/ViewController.swift
+++ TheRightDvorak/ViewController.swift
@@ -14,26 +14,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var instructions: UILabel!
     @IBOutlet weak var privacyPolicy: UILabel!
     
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            // Do any additional setup after loading the view, typically from a nib.
-            instructions.text = """
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        instructions.text = """
             How to use this keyboard:
              1. Open Settings
              2. Go to General > Keyboard > Keyboards
              3. Click "Add New Keyboard"
              4. Select "Right Dvorak"
              5. Congrats! You should now be able to access this keyboard on this device. Press the 🌐 key to switch between keyboards.
+            
+            6. In order to use haptics, you also need to "Allow Full Access" by selecting "Dvorak-Right-Handed__Right Dvorak" and hitting the toggle.
+            
+            View our privacy policy anytime by visiting our website:taylorcodeshop.com/theRightDvorak
             """
-            privacyPolicy.text = """
-            Privacy Policy:
-            Privacy is very important to us. Right Dvorak collects user information to provide user settings that remain unchanged
-            when the user closes and reopens our keyboard. We do not share user data with anyone.
-            User data is collected and stored locally on the device. We will update this document
-            if our data collection changes in any way. Anyone can access this document, our privacy
-            policy, by visiting our website: http://taylorcodeshop.com/theRightDvorak
-            """
-            let htmlString = "This is a <b>bold</b> text."
+
+        let htmlString = """
+<strong>The Right Dvorak App Information</strong>
+<br />
+<p>
+The Right Dorvak is an open source project created and maintained by me, Taylor Shaw. I use the Dvorak-Right-Handed keyboard on my personal computer. I wanted to be able to use the same keyboard on my iPad, so I created the app.
+</p>
+<b>Privacy Policy</b>
+<br />
+<p>
+Privacy is very important to us. We started collectig user data with app version 2.0. We collect minimal data in order to store your settings preferences. We store this data locally; we do not transmit any data over the network.
+</p>
+<p>
+Another important change with app version 2.0 and later, is that we request Full Access. By pressing "Allow Full Access", you are granting the developer access to certain systems, such as Location Servics and Address Book. We need Full Acess so that we can accass your device's haptic generator in order to provide haptic feedback. If you are not using haptics with this keyboard, whether or not you Allow Full Access makes no difference.
+</p>
+<b>Support</b>
+    <br />
+    <p>
+    We offer no support for this product. You can email tay@taylorcodeshop.com if you would like to notify the developer of any bugs.
+    </p>
+"""
             let data = htmlString.data(using: .utf8)!
             let attributedString = try? NSAttributedString(
                 data: data,
