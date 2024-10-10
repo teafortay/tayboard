8dfe1d966ba15ec01591ebd3892a9c7f616f2d57
Taylor Shaw <tay@taylorcodeshop.com>
2024-10-10 12:02:57 -0400

    setting up right kvc, deleting container folder around key titles

---
+++ Dvorak-Right-Handed/RightKeyboardViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RightKeyboardViewController.swift
+//  TheRightDvorak
+//
+//  Created by Taylor Shaw on 10/10/24.
+//  Copyright © 2024 Taylor Shaw. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RightKeyboardViewController: KeyboardViewController {
+    
+    @IBOutlet var nextKeyboardButton: UIButton!
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        
+        // Add custom view sizing constraints here
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
